5c7076938f74b1c36d118e2a18861e2f8f5c5d0d
amberhammer <amberhammer@hotmail.com>
2026-05-12 21:19:20 -0700

    Setup database connection

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
+require("dotenv").config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const db = require('./db');
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API running");
+db.query("SELECT NOW()")
+  .then(() => console.log("Database connected"))
+  .catch(err => console.error("DB connection failed", err));
+
+app.get("/test-db", async (req, res) => {
+  const result = await require("./db").query("SELECT NOW()");
+  res.json(result.rows);
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
