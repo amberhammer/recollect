cda7d4f6eb92ff2faea2968b1913800fa273e761
amberhammer <amberhammer@hotmail.com>
2026-05-18 11:21:22 -0700

    Protected library routes with auth middleware

--- server/routes/libraryRoutes.js
+++ server/routes/libraryRoutes.js
@@ -4,14 +4,12 @@ const router = express.Router();
 const { searchBooks, addToLibrary, getAllBooks, getBookById, updateBook, deleteBook } = require("../controllers/libraryController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.use(authMiddleware);
+router.get("/search", authMiddleware, searchBooks);
 
-router.get("/search", searchBooks);
-
-router.get("/", getAllBooks);
-router.get("/:id", getBookById);
-router.post("/", addToLibrary);
-router.put("/:id", updateBook);
-router.delete("/:id", deleteBook);
+router.get("/", authMiddleware, getAllBooks);
+router.get("/:id", authMiddleware, getBookById);
+router.post("/", authMiddleware, addToLibrary);
+router.put("/:id", authMiddleware, updateBook);
+router.delete("/:id", authMiddleware, deleteBook);
 
 module.exports = router;
