39c718b541203d262e5c37851e87d7b8971cca66
amberhammer <amberhammer@hotmail.com>
2026-05-18 19:55:38 -0700

    Set up theme provider and browser router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter } from 'react-router-dom';
+import theme from './theme.js';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
