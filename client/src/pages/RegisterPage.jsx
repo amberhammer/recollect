f11525002b9d4e9193fc49cbeb1f7a87fc67b567
amberhammer <amberhammer@hotmail.com>
2026-05-23 21:47:49 -0700

    Connected register page to auth API

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,42 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
 export default function RegisterPage() {
+  const navigate = useNavigate();
+
+  const { register } = useAuth();
+
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    try {
+      await register(formData);
+      navigate("/login");
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="flex flex-col items-center justify-center">
@@ -7,11 +45,11 @@ export default function RegisterPage() {
       </div>
     <div className="w-full max-w-md bg-white p-8 rounded-xl shadow-lg">
       <h1 className="text-3xl font-bold mb-6 text-center">Register</h1>
-      <form className="flex flex-col gap-4">
-        <input type="text" id="username" name="username" placeholder="Username" required className="border p-3 rounded-lg"/>
-        <input type="email" id="email" name="email" placeholder="Email" required className="border p-3 rounded-lg"/>
-        <input type="password" id="password" name="password" placeholder="Password" required className="border p-3 rounded-lg"/>
-        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" required className="border p-3 rounded-lg"/>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input type="text" id="username" name="username" placeholder="Username" required className="border p-3 rounded-lg" value={formData.username} onChange={handleChange}/>
+        <input type="email" id="email" name="email" placeholder="Email" required className="border p-3 rounded-lg" value={formData.email} onChange={handleChange}/>
+        <input type="password" id="password" name="password" placeholder="Password" required className="border p-3 rounded-lg" value={formData.password} onChange={handleChange}/>
+        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" required className="border p-3 rounded-lg" value={formData.confirmPassword} onChange={handleChange}/>
         <button className="bg-black text-white p-3 rounded-lg hover:bg-gray-800">Register</button>
       </form>
       <p className="text-sm text-gray-600 mt-4 text-center">Already have an account? <a href="/login" className="text-blue-500 hover:underline">Log in</a></p>
