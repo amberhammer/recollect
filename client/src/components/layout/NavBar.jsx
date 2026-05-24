c1c2588c642d72b27bd919288ecd5c1e3b774221
amberhammer <amberhammer@hotmail.com>
2026-05-24 15:59:19 -0700

    Updated header and footer styles

--- client/src/components/layout/NavBar.jsx
+++ client/src/components/layout/NavBar.jsx
@@ -3,14 +3,14 @@ import SearchBar from "./SearchBar";
 export default function NavBar() {
   return (
     <header>
-        <nav className="bg-gray-800 text-white p-4">
+        <nav className="bg-taupe-200 text-black p-4">
             <div className="container mx-auto flex items-center justify-between">
                 <a href="/" className="text-2xl font-bold">recollect</a>
                 <SearchBar />
                 <div className="flex items-center gap-4">
-                    <button className="bg-gray-700 px-3 py-2 rounded-lg hover:bg-gray-600">Friends</button>
-                    <button className="bg-gray-700 px-3 py-2 rounded-lg hover:bg-gray-600">Notifications</button>
-                    <button className="bg-gray-700 px-3 py-2 rounded-lg hover:bg-gray-600">Profile</button>
+                    <button className="bg-taupe-300 px-3 py-2 rounded-lg hover:border hover:border-taupe-400">Friends</button>
+                    <button className="bg-taupe-300 px-3 py-2 rounded-lg hover:border hover:border-taupe-400">Notifications</button>
+                    <button className="bg-taupe-300 px-3 py-2 rounded-lg hover:border hover:border-taupe-400">Profile</button>
                 </div>
             </div>
         </nav>
