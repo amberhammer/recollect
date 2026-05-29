b92ad0a1f103b465dcc30c5a81415e92736c2b3f
amberhammer <amberhammer@hotmail.com>
2026-05-28 21:15:54 -0700

    Collection page spacing issue fix

--- client/src/pages/CollectionPage.jsx
+++ client/src/pages/CollectionPage.jsx
@@ -123,7 +123,7 @@ export default function CollectionPage() {
             <div className="flex-grow flex justify-center p-4">
                 <div>
                     <h2 className="text-2xl font-bold mb-6 text-center">{displayName}</h2>
-                    <div className="grid grid-cols-2 gap-5 h-[550px] w-[700px] overflow-y-auto">
+                    <div className="grid grid-cols-2 gap-5 max-h-[570px] w-[700px] overflow-y-auto no-scrollbar">
                         {filteredBooks.map((book) => (
                             <BookCard
                                 key={book.id}
