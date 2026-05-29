aa03cad689a6a6b69b316f70dca509e6d6a5826f
amberhammer <amberhammer@hotmail.com>
2026-05-28 20:45:17 -0700

    Added nav links to home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import Cabinet from "../components/books/Cabinet";
 import Footer from "../components/layout/Footer";
 import NavBar from "../components/layout/NavBar";
@@ -11,12 +13,24 @@ export default function HomePage() {
                 <div>
                     <h2 className="text-2xl font-bold mb-6 text-center">your library</h2>
                     <div className="grid grid-cols-2 gap-5 h-[550px] w-[700px]">
-                        <Cabinet collectionName="All Books" />
-                        <Cabinet collectionName="Favourites" />
-                        <Cabinet collectionName="Currently Reading" />
-                        <Cabinet collectionName="To Be Read" />
-                        <Cabinet collectionName="Borrowed" />
-                        <Cabinet collectionName="Borrowing" />
+                        <Link to="/library" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <Cabinet collectionName="All Books" />
+                        </Link>
+                        <Link to="/library/favorites" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <Cabinet collectionName="Favourites" />
+                        </Link>
+                        <Link to="/library/currently-reading" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <Cabinet collectionName="Currently Reading" />
+                        </Link>
+                        <Link to="/library/to-read" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <Cabinet collectionName="To Be Read" />
+                        </Link>
+                        <Link to="/library/borrowed" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <Cabinet collectionName="Borrowed" />
+                        </Link>
+                        <Link to="/library/borrowing" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <Cabinet collectionName="Borrowing" />
+                        </Link>
                     </div>
                 </div>
             </div>
