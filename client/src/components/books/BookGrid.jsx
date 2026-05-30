a8b2767b42857e7cbe0eb8d6ebb8ae75a0616eab
amberhammer <amberhammer@hotmail.com>
2026-05-30 12:01:43 -0700

    Clickable book card on collection page

--- client/src/components/books/BookGrid.jsx
+++ client/src/components/books/BookGrid.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import BookCard from "./BookCard";
 
 export default function BookGrid({ displayName, books }) {
@@ -7,12 +8,13 @@ export default function BookGrid({ displayName, books }) {
                 <h2 className="text-2xl font-bold mb-6 text-center">{displayName}</h2>
                 <div className="grid grid-cols-2 gap-5 max-h-[570px] w-[700px] overflow-y-auto no-scrollbar">
                     {books.map((book) => (
-                        <BookCard
-                            key={book.id}
-                            title={book.title}
-                            author={book.authors[0]}
-                            thumbnail={book.thumbnail}
-                        />
+                        <Link key={book.id} to={`/books/${book.google_books_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <BookCard
+                                title={book.title}
+                                author={book.authors[0]}
+                                thumbnail={book.thumbnail}
+                            />
+                        </Link>
                     ))}
                 </div>
             </div>
